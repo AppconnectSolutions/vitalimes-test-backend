fa8808b4de2936a66474cd8b32f964b834cb442f
AppconnectSolutions <Hr@appconnectsolutions.com>
2026-01-21 11:52:25 +0530

    server commit

--- server.js
+++ server.js
@@ -15,7 +15,19 @@ import axios from "axios";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: [
+    "https://appconnect.cloud",
+    "https://api.appconnect.cloud"
+  ],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
+// IMPORTANT: handle preflight requests
+app.options("*", cors());
+
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
